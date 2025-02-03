f5b16208b0abd46f2ba19e46251efa0387886fdc
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-03 12:02:13 +0200

    add test state of dice

--- __tests__/dice.test.js
+++ __tests__/dice.test.js
@@ -2,33 +2,44 @@
 
 const Dice = require('../dice');
 
-describe('teste for the constructor', () => {
-    describe('with no upper bound given', () => {
+
+describe('tests for the constructor', () => {
+
+    describe('with no upperBound given', () => {
         const dice = new Dice();
 
         test('minimumValue is 1', () => {
             expect(dice.minimumValue).toBe(1);
         });
+
         test('maximumValue is 6', () => {
-            expect(dice.maximumValue).toBe(6); // getters don't need paranthesis after
+            expect(dice.maximumValue).toBe(6);  // getters don't need paranthesis after
         });
+
         test('dots is 0', () => {
             expect(dice.dots).toBe(0);
-        })
+        });
+
+        test('state of dice ok', () => {
+            expect(dice.minimumValue).toBe(1);
+            expect(dice.maximumValue).toBe(6);
+            expect(dice.dots).toBe(0);
+        });
     });
 
     describe('with upper bounds 2-20', () => {
         const testValues =
             new Array(19).fill(2).map((value, ind) => [value + ind]);
 
-        test.each(testValues)('Test upperbound %s', upBound => {
-            const dice = new Dice(upBound);
+        test.each(testValues)('Test upperbound %s', ubound => {
+            const dice = new Dice(ubound);
             expect(dice.minimumValue).toBe(1);
             expect(dice.dots).toBe(0);
-            expect(dice.maximumValue).toBe(upBound);
-        })
+            expect(dice.maximumValue).toBe(ubound);
+        });
     });
-    describe('Test the exceptions', () => {
+
+    describe(' Test the exceptions', () => {
         const testValues = [
             ['a', 'upper bound must be an integer'],
             ['1', 'upper bound must be an integer'],
@@ -41,83 +52,78 @@ describe('teste for the constructor', () => {
             [-1, 'upper bound too small'],
             [21, 'upper bound too big']
         ];
-        test.each(testValues)('upperBound %s throws %s ', (ub, text) => {
 
+        test.each(testValues)('upperBound %s throws %s', (ub, text) => {
             expect(() => new Dice(ub)).toThrow(text);
         });
     });
+}); //end of constructortests
 
-    describe('Test with undefined upper bound', () => {
-        test('Defaults to 6 when upper bound is undefined', () => {
-            const dice = new Dice(undefined);
-            expect(dice.minimumValue).toBe(1);
-            expect(dice.maximumValue).toBe(6);
-            expect(dice.dots).toBe(0);
+describe('Testing roll', () => {
+    describe('default upperbound', () => {
+        const dice = new Dice();
+
+        test('when rolled', () => {
+            dice.roll();
+            expect(dice.dots).toBeGreaterThanOrEqual(1);
+            expect(dice.dots).toBeLessThanOrEqual(6);
+        });
+    });
+    describe('default upperbound v2', () => {
+        const dice = new Dice();
+        dice.roll();
+
+        test('when rolled >=1', () => {
+            expect(dice.dots).toBeGreaterThanOrEqual(1);
+        });
+        test('when rolled <=6', () => {
+            expect(dice.dots).toBeLessThanOrEqual(6);
         });
     });
 
-    describe('Testing roll', () => {
-        describe('default upperbound', () => {
-            const dice = new Dice();
-            test('when rolled', () => {
-                dice.roll();
-                expect(dice.dots).toBeGreaterThanOrEqual(1);
-                expect(dice.dots).toBeLessThanOrEqual(6);
-            });
-        });
+    describe('upperbounds 2...20', () => {
+        const testValues =
+            new Array(19).fill(2).map((value, ind) => [value + ind]);
 
-        describe('default upperbound version2', () => {
-            const dice = new Dice();
+        test.each(testValues)('test upper bound %s', (ubound) => {
+            const dice = new Dice(ubound);
             dice.roll();
-            test('when rolled >=1', () => {
-                expect(dice.dots).toBeGreaterThanOrEqual(1);
-            });
-            test('when rolled <=6', () => {
-                expect(dice.dots).toBeLessThanOrEqual(6);
-            });
+            expect(dice.dots).toBeGreaterThanOrEqual(1);
+            expect(dice.dots).toBeLessThanOrEqual(ubound);
         });
 
-        describe('upperbounds 2...20', () => {
-            const testValues =
-                new Array(19).fill(2).map((value, ind) => [value + ind]);
+    });
+});//end of testing roll
 
-            test.each(testValues)('Test upperbound %s', upBound => {
-                const dice = new Dice(upBound);
+describe('testing toString', () => {
+    let dice;
 
-                dice.roll();
-                expect(dice.dots).toBeGreaterThanOrEqual(1);
-                expect(dice.dots).toBeLessThanOrEqual(upBound);
+    beforeEach(() => {
+        dice = new Dice();
+    });
 
-            });
-        });
+    test('dice rolled', () => {
+        dice.roll();
+        expect(dice.toString()).toBe(`${dice.dots}`);
+    });
 
-        describe('testing toString', () => {
-            let dice;
-            beforeEach(() => {
-                dice = new Dice();
-            });
-            test('dice rolled', () => {
-                dice.roll();
-                expect(dice.toString()).toBe(`${dice.dots}`);
-            });
-
-            test('dice not rolled', () => {
-                expect(dice.toString()).toBe('not rolled yet');
-            });
-        });
-    }); // das hier ist eins zu viel (falsch genested?) vielleicht auch nicht?
-})
+    test('dice not rolled', () => {
+        expect(dice.toString()).toBe('not rolled yet');
+    });
+});
 
 describe('test for roll sequencies', () => {
     describe('create dice with no upper bound given', () => {
         const dice = new Dice();
-        for (let i = 0; i < 10; i++) {
+
+        for (let i = 0; i < 100; i++) {
             test(`${i}th roll`, () => {
                 dice.roll();
                 expect(dice.dots).toBeGreaterThanOrEqual(1);
                 expect(dice.dots).toBeLessThanOrEqual(6);
             });
         }
+
         const dotCounts = [];
         test('dot distribution ok', () => {
             for (let i = 0; i < 60; i++) {
@@ -125,6 +131,6 @@ describe('test for roll sequencies', () => {
                 dotCounts.push(dice.dots);
             }
             expect(new Set(dotCounts).size).toBe(6);
-        })
+        });
     });
 });
